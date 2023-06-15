3b26f13e01fbec359db4725ef14f1de391792e53
Kesav2716 <135842007+Kesav2716@users.noreply.github.com>
2023-06-15 20:02:30 +0530

    Update powershelltest.ps1

--- powershelltest.ps1
+++ powershelltest.ps1
@@ -5,4 +5,4 @@ param (
 
 Write-Host "Parameter 1: $param1"
 Write-Host "Parameter 2: $param2"
-Write-Host "##gbStart##Data1##spiltKeyValue##TestingAppstore##gbEnd"
+Write-Host "##gbStart##Data1##splitKeyValue##TestingAppstore##gbEnd"

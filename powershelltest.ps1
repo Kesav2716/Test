bce3bfdb383f67b46454a66c8d1b3cd2dec52985
Kesav2716 <135842007+Kesav2716@users.noreply.github.com>
2023-06-17 16:02:18 +0530

    Update powershelltest.ps1

--- powershelltest.ps1
+++ powershelltest.ps1
@@ -1,12 +1,3 @@
-<#
-  * To store output of step to Global variables use the below format in the log statement of respective language
-  * ##gbStart##Key##splitKeyValue##Value##gbEnd##
-  * ##gbStart## - indicates Global variable declaration is starting.
-  * Key - Your variable Key to be stored in Global variables
-  * splitKeyValue - to split key and value
-  * value - Your variable value to be stored in Global variables
-  * ##gbEnd## - Global variable declaration ends
-  #>
 param (
     $param1,
     $param2

5b250ef25e295fa66f35a929aaed471dc67b277a
Kesav2716 <135842007+Kesav2716@users.noreply.github.com>
2023-06-20 11:07:06 +0530

    Update Appstore.ps1

--- Appstore.ps1
+++ Appstore.ps1
@@ -5,4 +5,4 @@ param (
 
 Write-Host "Parameter 1: $param3"
 Write-Host "Parameter 2: $param4"
-Write-Host "##gbStart##Appstore##splitKeyValue##TestingAppstore##gbEnd"
+Write-Host "##gbStart##Appstore##splitKeyValue##TestingAppstore##gbEnd##"

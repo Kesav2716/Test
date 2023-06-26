abb94b8340f2963b93d86380d591837a43d57b08
Kesav2716 <135842007+Kesav2716@users.noreply.github.com>
2023-06-26 19:34:18 +0530

    Create Folder.ps1

---
+++ Folder.ps1
@@ -0,0 +1,7 @@
+$folderPath = "D:\NewFolder"
+if (-not (Test-Path $folderPath)) {
+    New-Item -ItemType Directory -Path $folderPath
+    Write-Host "Folder created successfully."
+} else {
+    Write-Host "Folder already exists."
+}

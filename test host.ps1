865900a977680e44b0ab1fff4120950e05cbfdc4
Kesav2716 <135842007+Kesav2716@users.noreply.github.com>
2023-07-26 15:07:35 +0530

    Create test host.ps1

---
+++ test host.ps1
@@ -0,0 +1,6 @@
+param (
+    $ipneedchange,
+    $newip
+)
+
+(Get-Content -Path "C:\Windows\System32\drivers\etc\hosts") | ForEach-Object {$_ -replace $ipneedchange, $newip} | Set-Content -Path "C:\Windows\System32\drivers\etc\hosts"
